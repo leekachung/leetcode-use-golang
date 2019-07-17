5c95e2ca9fb3f8893191146d7650e2c0b71291a4
leekachung <leekachung17@gmail.com>
2019-07-17 21:25:52 +0800

    add T48 solution 2

--- medium/48.go
+++ medium/48.go
@@ -1,3 +1,4 @@
+// Solution 1
 func rotate(matrix [][]int)  {
     // 对角线翻转
     length := len(matrix)
@@ -12,4 +13,20 @@ func rotate(matrix [][]int)  {
             matrix[j][i], matrix[j][length-i-1] = matrix[j][length-i-1], matrix[j][i]
         }
     }
+}
+
+// Solution 2
+// 分别将四个顶点顺时针旋转
+func rotate(matrix [][]int)  {
+    n := len(matrix)
+	len := n-1
+	for i := 0; i < n/2; i++ {
+		for j := i; j < len-i; j++ {
+			tmp := matrix[i][j]
+			matrix[i][j] = matrix[len-j][i]
+			matrix[len-j][i] = matrix[len-i][len-j]
+			matrix[len-i][len-j] = matrix[j][len-i]
+			matrix[j][len-i]  = tmp
+		}
+	}
 }
