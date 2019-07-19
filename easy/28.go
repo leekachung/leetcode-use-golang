9b20410c0cf6917bae7c68874a0780d6e3be9106
leekachung <leekachung17@gmail.com>
2019-07-19 14:29:22 +0800

    fixed T28 solution

--- easy/28.go
+++ easy/28.go
@@ -2,10 +2,11 @@ func strStr(haystack string, needle string) int {
     n_length := len(needle)
     h_length := len(haystack)
     if n_length == 0 { return 0 }
+    // 当needle长度大于haystack 可判定包含关系不存在
+    if  n_length > h_length { return -1 }
     for i, _ := range haystack {
-        // 当needle长度大于haystack 以及定位后的第一个字母索引加上needle长度大于haystack 
-        // 可判定包含关系不存在
-        if n_length > h_length || i + n_length > h_length{
+        // 定位后的第一个字母索引加上needle长度大于haystack 可判定包含关系不存在
+        if i + n_length > h_length{
             break
         }
         if haystack[i:i+n_length] == needle {
