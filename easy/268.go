0bba90de323994350a68fa4112b635b18a885de1
leekachung <leekachung17@gmail.com>
2019-07-26 11:46:51 +0800

    add T268 solution 2 && 3

--- easy/268.go
+++ easy/268.go
@@ -1,3 +1,4 @@
+// solution 1 不适合数字重复出现的场景
 func missingNumber(nums []int) int {
     sum := 0
     total := len(nums)
@@ -7,3 +8,27 @@ func missingNumber(nums []int) int {
     }
     return total - sum
 }
+
+// solution 2 适合数字随机重复出现的场景 
+func missingNumber(nums []int) int {
+    m := make([]byte, len(nums) + 1)
+    for _, v := range nums {
+        m[v] = 1
+    }
+    for k, v := range m {
+        if v < 1 {
+            return k
+        }
+    }
+    return -1    
+}
+
+// solution 3 适合数字以奇数次重复出现的场景
+func missingNumber(nums []int) int {
+    res := len(nums)
+    for i := 0; i < len(nums); i++ {
+        res ^= i ^ nums[i]
+    }
+    return res
+}
+
